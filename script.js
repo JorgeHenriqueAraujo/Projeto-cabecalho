4a840a3106499bfc353716298c8c2bbf17650154
JorgeHenriqueAraujo <142536858+JorgeHenriqueAraujo@users.noreply.github.com>
2025-05-02 13:01:38 -0300

    .

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function mostrarSecao(secao) {
     document.querySelectorAll('.conteudo').forEach(el => el.style.display = 'none');
     document.getElementById(secao).style.display = 'block';
 }
+
+function alternarAcessibilidade() {
+    document.body.classList.toggle('modo-acessibilidade');
+}
